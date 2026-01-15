d39e29287aa9731c09534cc498fe451fff52d7ad
naphalm <master.sys.vp@gmail.com>
2026-01-15 14:04:07 +0200

    update go.main

--- app/main.go
+++ app/main.go
@@ -7,17 +7,22 @@ import (
 	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found")
+	}
+
 	db.Init()
 
-	r:= gin.Default()
+	r := gin.Default()
 
 	r.GET("/health", handlers.Health)
 	r.POST("/register", handlers.Register)
 	r.POST("/login", handlers.Login)
-	r.GET("validate", handlers.JWTAuthMiddleware(), handlers.Validate)
+	r.GET("/validate", handlers.JWTAuthMiddleware(), handlers.Validate)
 
 	port := os.Getenv("PORT")
 	if port == "" {
